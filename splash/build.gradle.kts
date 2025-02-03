421a6381a246f2447956e8de033edf90f7005f0e
= <=>
2025-02-03 23:00:04 +0300

    * :splash(groovy to kts)

---
+++ splash/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id ("com.android.library")
+    id ("kotlin-android")
+    id ("kotlin-kapt")
+}
+
+android {
+    configureAndroidLibrary(namespace = "com.example.splash")
+}
+
+dependencies {
+
+    implementation(project(":core"))
+    implementation(project(":asset"))
+
+    implementation(Dependencies.ANDROID_CORE)
+    implementation(Dependencies.APP_COMPAT)
+    implementation(Dependencies.MATERIAL)
+    testImplementation(Dependencies.JUNIT)
+    androidTestImplementation(Dependencies.ANDROID_JUNIT)
+    androidTestImplementation(Dependencies.ANDROID_ESPRESSO)
+
+
+
+    implementation(Dependencies.CONSTRAINT_LAYOUT)
+    implementation(Dependencies.RECYCLER_VIEW)
+
+    implementation(Dependencies.DAGGER)
+    kapt(Dependencies.DAGGER_COMPILER)
+
+    Dependencies.navigations.forEach(::implementation)
+
+    //viewdelegate
+    implementation(Dependencies.VIEW_BINDING_DELEGATE)
+}
